4e9bb2bf836d05c53a474ad44608972658bcffcf
Faust Yang <fausty@synnex.com>
2020-11-19 14:25:02 +0800

    figure 图像常见用法

--- 2.figure 图像.py
+++ 2.figure 图像.py
@@ -15,12 +15,14 @@ y2 = x**2
 
 # 定义一个figure, num=1表明这个窗口名字是figure1
 # 定义在某个figure之后的东西，都属于这个figure的操作
-plt.figure(num=1)
+plt.figure(num=1, figsize=(8, 5))
 # 属于figure1的操作
 plt.plot(x, y1)
 
 plt.figure(num=3)
 # 属于figure3的操作
 plt.plot(x, y2)
+# 画一条x 和 y1的线， 线的颜色是红色， 线条的宽度是1.0， 线条的格式是虚线
+plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
 
 plt.show()
